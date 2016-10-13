296efdd709051e1dad45968cb9b0003008283a22
Aurelien <aureliendumont@MacBook-Pro.local>
2016-10-13 23:40:50 +0200

    robot

---
+++ app/Robot.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+
+class Robot {
+
+	constructor(){
+
+		this.style = {}
+
+	}
+
+	setAction(actions){
+
+		this.actions = actions;
+
+	}
+
+	go(){
+
+		for(var i in this.actions){
+			
+		}
+
+	}
+
+	draw(){
+
+
+	}
+
+}
+
+export default Robot;
